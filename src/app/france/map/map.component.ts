cf1c2401899d19cf6a5e8f6199e2bb98ca32e150
Bastien <bastien_maurice@hotmail.com>
2020-03-29 20:44:04 +0200

    Link data geojson pays/dep/reg avec leaflet dynamiquement

--- src/app/france/map/map.component.ts
+++ src/app/france/map/map.component.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import * as leaflet from 'leaflet';
+import * as L from 'leaflet';
 import { HttpClient } from '@angular/common/http';
+import { ResizeEvent } from 'angular-resizable-element';
+import { EGranulariteCarte } from 'src/app/shared/enum/EGranulariteCarte';
+
+
+const MIN_WIDTH_SIDEBAR = 300;
+let OLD_WIDTH_SIDEBAR = 300;
 
 @Component({
     selector: 'app-map',
@@ -9,68 +15,116 @@ import { HttpClient } from '@angular/common/http';
 })
 export class MapComponent implements OnInit {
 
-    public map;
-    public geojson;
-    public info;
+    // MENU
+    public isOpenSidebar: boolean;
+    public widthSidebar: number;
+    public selectedGranularityMap: EGranulariteCarte;
+    public listAllGranularityMap: EGranulariteCarte[];
+
+    public map: L.Map;
+
+    public franceData;
+    public regionData;
+    public departementData;
+
     constructor(
         private http: HttpClient
     ) {
+        this.widthSidebar = OLD_WIDTH_SIDEBAR;
+        this.isOpenSidebar = true;
+        this.selectedGranularityMap = EGranulariteCarte.PAYS;
+        this.listAllGranularityMap = [EGranulariteCarte.PAYS, EGranulariteCarte.REGION, EGranulariteCarte.DEPARTEMENT];
 
+    }
+
+
+
+    public toggleSidebar(): void {
+        this.isOpenSidebar = !this.isOpenSidebar;
+        if (this.isOpenSidebar) {
+            this.widthSidebar = OLD_WIDTH_SIDEBAR;
+        } else {
+            this.widthSidebar = 30;
+        }
+    }
+
+    public onResizeEnd(event: ResizeEvent): void {
+        OLD_WIDTH_SIDEBAR = event.rectangle.width;
+        this.widthSidebar = event.rectangle.width;
+    }
+
+    public validate(event: ResizeEvent): boolean {
+        if (
+            event.rectangle.width &&
+            (event.rectangle.width < MIN_WIDTH_SIDEBAR)
+        ) {
+            return false;
+        }
+        return true;
     }
 
     ngOnInit(): void {
-        this.map = leaflet.map('map').setView([47.0708191, 1.2772772], 6);
-        leaflet.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        this.map = L.map('map').setView([46.303558, 6.0164252], 6);
+        L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
             id: 'mapbox',
             attribution: 'Bastien MAURICE'
         }).addTo(this.map);
-        this.info = new leaflet.Control();
 
-        function resetHighlight(e) {
-            this.geojson.resetStyle(e.target);
-            this.info.update();
-        }
-    
-        function zoomToFeature(e) {
-            this.map.fitBounds(e.target.getBounds());
-        }
-    
-        function highlightFeature(e) {
-            const layer = e.target;
-    
-            layer.setStyle({
-                weight: 5,
-                color: "#666",
-                dashArray: "",
-                fillOpacity: 0.2
-            });
-    
-            if (!leaflet.Browser.ie && !leaflet.Browser.edge) {
-                layer.bringToFront();
-            }
-        }
-
-        this.http.get('assets/geo/france/region.json').subscribe((json: any) => {
-            this.geojson = leaflet.geoJSON(json, {
-                style: {
-                    color: "white",
-                    fillColor: "red",
-                    fillOpacity: 0.1
-                },
+        this.http.get('assets/geo/france/PAYS.json').subscribe((json: any) => {
+            this.franceData = L.geoJSON(json, {
                 onEachFeature: function onEachFeature(feature, layer) {
-
-                    layer.on({
-                        mouseover: highlightFeature,
-                        mouseout: resetHighlight,
-                        click: zoomToFeature
-                    });
                 }
-            }).addTo(this.map);
+            });
+            this.map.addLayer(this.franceData)
         });
 
+        this.http.get('assets/geo/france/DEPARTEMENT.json').subscribe((json: any) => {
+            this.departementData = L.geoJSON(json, {
+                onEachFeature: function onEachFeature(feature, layer) {
+                }
+            });
+        });
 
+        this.http.get('assets/geo/france/REGION.json').subscribe((json: any) => {
+            this.regionData = L.geoJSON(json, {
+                onEachFeature: function onEachFeature(feature, layer) {
+                }
+            });
+        });
 
     }
+
+    public updateGranularity(): void {
+        switch(this.selectedGranularityMap) {
+            case EGranulariteCarte.PAYS: {
+                this.map.removeLayer(this.regionData);
+                this.map.removeLayer(this.departementData);
+                this.map.addLayer(this.franceData);
+
+                break;
+            }
+            case EGranulariteCarte.REGION: {
+                this.map.removeLayer(this.franceData);
+                this.map.removeLayer(this.departementData);
+                this.map.addLayer(this.regionData);
+
+                break;
+            }
+            case EGranulariteCarte.DEPARTEMENT: {
+                this.map.removeLayer(this.regionData);
+                this.map.removeLayer(this.franceData);
+                this.map.addLayer(this.departementData);
+
+                break;
+            }
+
+        }
+
+    
+    }
+
+
+
 
 
 
